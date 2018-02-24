328851ba76d8c072ac6b60beb5b54f7c2b13fb41
triamri <triamri@gmail.com>
2018-02-24 22:25:47 +0700

    setting cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
 
 mongoose.connect('mongodb://localhost/pry');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
